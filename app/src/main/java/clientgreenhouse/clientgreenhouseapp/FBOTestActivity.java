01e794ff136882f608444d51c7cc18279e19f74e
seanmahoney <seunmahoney@gmail.com>
2017-07-21 15:35:01 -0300

    Espresso tests for the graphs- Sean, Asma, and Ben

--- app/src/main/java/clientgreenhouse/clientgreenhouseapp/FBOTestActivity.java
+++ app/src/main/java/clientgreenhouse/clientgreenhouseapp/FBOTestActivity.java
@@ -1,7 +1,7 @@
 package clientgreenhouse.clientgreenhouseapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -9,8 +9,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
 
 public class FBOTestActivity extends AppCompatActivity {
 
